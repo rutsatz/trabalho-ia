680b21c8d48bcfbed5dafc8d9a2cc29dbe1b847c
m96627 <m96627@PC0623.acad.unisc.br>
2018-04-12 21:11:26 -0300

    Adicionado calculo para definir profunidade da arvore

--- src/agent/Agente.java
+++ src/agent/Agente.java
@@ -7,11 +7,12 @@ import avaliacao.PesoFolhaColocacaoPeca;
 
 public class Agente implements PlayerAgent {
 
-    public static final int PROFUNDIDADE = 4;
+    public static final int PROFUNDIDADE = 5;
 
     @Override
     public String setPiece(GameInfo info) {
-        MinimaxTree tree = new MinimaxTree(PROFUNDIDADE, MinimaxTree.SET_ACTION, info) {
+
+        MinimaxTree tree = new MinimaxTree(calcularProfundidade(info), MinimaxTree.SET_ACTION, info) {
             @Override
             public int scoreGameState(Node node) {
                 return score(info, node);
@@ -23,7 +24,7 @@ public class Agente implements PlayerAgent {
 
     @Override
     public String movePiece(GameInfo info) {
-        MinimaxTree tree = new MinimaxTree(PROFUNDIDADE, MinimaxTree.MOVE_ACTION, info) {
+        MinimaxTree tree = new MinimaxTree(calcularProfundidade(info), MinimaxTree.MOVE_ACTION, info) {
             @Override
             public int scoreGameState(Node node) {
                 return score(info, node);
@@ -35,7 +36,7 @@ public class Agente implements PlayerAgent {
 
     @Override
     public String removePiece(GameInfo info) {
-        MinimaxTree tree = new MinimaxTree(PROFUNDIDADE, MinimaxTree.REMOVE_ACTION, info) {
+        MinimaxTree tree = new MinimaxTree(calcularProfundidade(info), MinimaxTree.REMOVE_ACTION, info) {
             @Override
             public int scoreGameState(Node node) {
                 return score(info, node);
@@ -48,5 +49,39 @@ public class Agente implements PlayerAgent {
     //private int score( GameInfo info, int[][] gameState, int playerTurn, int piecesToPlace, int opponentPiecesToPlace )
     private int score(GameInfo info, MinimaxTree.Node node) {
         return PesoFolhaColocacaoPeca.avalia(node.getGameState());
+    }
+
+    public int calcularProfundidade(GameInfo info) {
+        int totalPecasOponente = info.getOpponentSpots().size();
+        int totalPecasAgente = info.getPlayerSpots().size();
+
+        boolean colocacaoPesas = info.getPiecesToPlace() == 0 ? false : true;
+        int totalPecas = totalPecasOponente + totalPecasAgente;
+
+        if (colocacaoPesas) {
+
+            if (totalPecas > 15) {
+                return 7;
+            }
+            if (totalPecas > 10) {
+                return 5;
+            }
+            if (totalPecas < 5) {
+                return 3;
+            }
+
+        } else {
+            if (totalPecas > 15) {
+                return 3;
+            }
+            if (totalPecas > 10) {
+                return 5;
+            }
+            if (totalPecas < 5) {
+                return 7;
+            }
+        }
+
+        return 3;
     }
 }
